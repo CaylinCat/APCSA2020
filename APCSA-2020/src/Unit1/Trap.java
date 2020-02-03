acab46a6307df98cfbda7dae9a6b588588582440
Caylin <canoyc7624@CA-SU-F106-23.sduhsd.lan>
2020-02-03 10:37:51 -0800

    Unit 1

---
+++ APCSA-2020/src/Unit1/Trap.java
@@ -0,0 +1,14 @@
+package Unit1;
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return (double)(0.5*(base1 + base2)*height);
+	}
+}
