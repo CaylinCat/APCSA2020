2f58fa277c3d5b2755dcc30ad7964dc279b15e62
CaylinCat <60459726+CaylinCat@users.noreply.github.com>
2020-03-04 22:44:02 -0800

    Did stuff - home

--- APCSA-2020/src/Unit10/ToyStore.java
+++ APCSA-2020/src/Unit10/ToyStore.java
@@ -19,20 +19,68 @@ public class ToyStore
 
 	public void loadToys( String toys )
 	{
+		String singleToy = "";
+		String toyss = toys;
+		while(toyss.indexOf(" ") > -1){
+			singleToy = toyss.substring(0,toyss.indexOf(" "));
+			toyss = toyss.substring(toyss.indexOf(" "));
+			toyList.add(singleToy);
+		}
 	}
   
   	public Toy getThatToy( String nm )
   	{
-  		return null;
+  		for(Toy hi : toyList){
+			if(hi.getname().equals(nm))
+				return hi;
+		}
+		return "";
   	}
   
   	public String getMostFrequentToy()
   	{
-  		return "";
+		int count = 0;
+		int greatestCount = 0;
+		String freq = "";
+  		for(Toy hi : toyList){
+			for(int i=0; i<toyList.size(); i++){
+				if(hi.getname()equals(nm))
+					count++;
+			}
+			if(count > greatestCount)
+				greatestCount = count;
+				freq = hi.getname();
+		}
+		return freq;
   	}  
   
   	public void sortToysByCount()
   	{
+		int count = 0;
+		ArrayList<Integer> index = new ArrayList<Integer>();
+		ArrayList<Toy> newBox = new ArrayList<Toy>();
+		String freq = "";
+  		for(Toy hi : toyList){
+			for(int i=0; i<toyList.size(); i++){
+				if(hi.getname()equals(nm))
+					count++;
+			}
+			freq = hi.getname();
+			if(index.size()==0)
+				newBox.add(freq);
+				index.add(count);
+			if(index.size()>0){
+				for(int i=0; i<newBox.size(); i++){
+					if(count > index.get(i))
+						newBox.add(freq,i+1);
+					else if (count < index.get(i))
+						newBox.add(freq, 0);
+				}
+			}
+		}
+		for(int i=0; i<toyBox.size(); i++){
+			toyList.get() = newBox.get();
+		}
   	}  
   	  
 	public String toString()
