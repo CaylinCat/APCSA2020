1e009b467b86895eff6337f525695b39ef0ee0e3
Caylin <caylin.email@gmail.com>
2020-03-12 11:18:18 -0700

    more farming markety - school

--- APCSA-2020/src/DataLab/FarmersMarketRunner.java
+++ APCSA-2020/src/DataLab/FarmersMarketRunner.java
@@ -7,8 +7,10 @@ import core.data.*;
 public class FarmersMarketRunner {
    public static void main(String[] args) {
 	   DataSource ds = DataSource.connect("https://data.baltimorecity.gov/api/views/atzp-3tnt/rows.xml").load();
-	      ArrayList<FarmersMarket> allmrk = ds.fetchList(FarmersMarket.class, "row/name", "row/day", "row/monthsopen", 
-	    		  "row/monthsclose", "row/openingtime", "row/closingtime", "row/numberofvendors");
+	   ds.printUsageString();
+	      ArrayList<FarmersMarket> allmrk = ds.fetchList(FarmersMarket.class, "row/row/name", "row/row/day_", "row/row/monthsopen", 
+	    		  "row/row/monthsclose", "row/row/openingtime", "row/row/closingtime", "row/row/numberofvendors");
+	      
 	      System.out.println("Total number of Farmer's Markets: " + allmrk.size());
 	      
 	      /*Scanner sc = new Scanner(System.in);
