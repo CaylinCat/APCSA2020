632ed5fe584a994ee6731df841caf3632c3114ec
Caylin <caylin.email@gmail.com>
2020-04-27 12:56:28 -0700

    Pong stuff 1 & 2

---
+++ APCSA-2020/src/Pong/BallTestTwo.java
@@ -0,0 +1,71 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+package Pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Canvas;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class BallTestTwo extends Canvas implements Runnable
+{
+	private Ball ball;
+
+	public BallTestTwo()
+	{
+		setBackground(Color.WHITE);
+		setVisible(true);
+
+		//instantiate a new Ball
+		Ball cat = new Ball();
+
+		//test the Ball thoroughly
+		System.out.println(cat);
+
+		//test all constructors
+		Ball dog = new Ball(20,20);
+		Ball uwu = new Ball(30,30,30,30);
+		Ball owo = new Ball(10,10,10,10, Color.RED);
+		Ball yee = new Ball(15,15,15,15,Color.blue,2,2);
+
+
+		new Thread(this).start();
+	}
+	
+	public void update(Graphics window)
+	{
+		paint(window);
+	}
+
+	public void paint(Graphics window)
+	{
+		ball.moveAndDraw(window);
+
+		if(!(ball.getX()>=10 && ball.getX()<=550))
+		{
+			ball.setXSpeed(-ball.getXSpeed());
+		}
+
+		if(!(ball.getY()>=10 && ball.getY()<=450))
+		{
+			ball.setYSpeed(-ball.getYSpeed());
+		}
+	}
+	
+   public void run()
+   {
+   	try
+   	{
+   		while(true)
+   		{
+   		   Thread.currentThread().sleep(19);
+            repaint();
+         }
+      }catch(Exception e)
+      {
+      }
+  	}	
+}
